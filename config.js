f401aab9e107acc6dc25f913c0dc444877313893
TheTheLOLMAN <76574626+TheTheLOLMAN@users.noreply.github.com>
2026-09-17 04:24:57 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 // Easy to change — password is case-sensitive
 window.SITE_CONFIG = {
   password: "northernstar",
-  youtubeId: "cwjmoTXkqUI",
   unlockKey: "hoppou_unlocked",
+  unlockPage: "levelone.html",
   message: "congratulations, you are now heading into something deeper.",
   messageColor: "#e34c3e",
   footerMessage: "Look up and search the night sky",
